55918226a94b78e928beaa0e4b3c204165716544
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-02-19 09:41:42 -0800

    chore: fix types

--- lib/types.ts
+++ lib/types.ts
@@ -1,5 +1,5 @@
 export interface IPreviewProgram {
-  thumb: string
+  imagePath: string
   title: string
   authorKaid: string
   created: string
@@ -17,20 +17,18 @@ export interface IProgram {
     avatarSrc: string
     nickname: string
   }
-  scratchpad: {
-    height: number
-    width: number
-    id: number
-    revision: {
-      code: string
-    }
-    spinoffCount: number
-    sumVotesIncremented: number
-    title: string
-    type: string
-    userAuthoredContentType: string
-    url: string
+  height: number
+  width: number
+  id: number
+  revision: {
+    code: string
   }
+  spinoffCount: number
+  sumVotesIncremented: number
+  title: string
+  type: string
+  userAuthoredContentType: string
+  url: string
 }
 
 export interface IProfile {
